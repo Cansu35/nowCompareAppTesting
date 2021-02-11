8abc76d5e8c3ce814fe54d393ed767ef7d960e22
Mustafa Cihat YAPICI <cyapicii@gmail.com>
2021-02-11 01:41:06 +0100

    we change the method structure in BasePage.java

---
+++ src/test/java/com/nowcompare/step_definitions/LifeStepDefs.java
@@ -0,0 +1,41 @@
+package com.nowcompare.step_definitions;
+
+import com.nowcompare.pages.Dashboard;
+import com.nowcompare.pages.LifePage;
+import com.nowcompare.pages.SavingsPage;
+import com.nowcompare.utilities.ConfigurationReader;
+import com.nowcompare.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LifeStepDefs {
+
+    @Given("the user in application dashboard")
+    public void the_user_in_application_dashboard() {
+        Driver.get().get(ConfigurationReader.get("url"));
+    }
+
+    @When("the user go to the Insurance-Life")
+    public void the_user_go_to_the_Insurance_Life() {
+        new Dashboard().navigateTo("Insurance","Life");
+
+    }
+
+    @Then("verify the page consist of Life Insurance")
+    public void verify_the_page_consist_of_Life_Insurance() {
+        Assert.assertEquals("Life Insurance", new LifePage().lifeText.getText());
+    }
+
+    @When("the user can navigate to {string} and {string}")
+    public void the_user_can_navigate_to_and(String tab, String module) {
+        new Dashboard().navigateTo(tab,module);
+    }
+
+    @Then("verify the page consist of Savings")
+    public void verify_the_page_consist_of_Savings() {
+        Assert.assertEquals("Savings", new SavingsPage().savingsText.getText());
+    }
+
+}
